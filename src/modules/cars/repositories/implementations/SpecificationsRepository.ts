33032d6604055b8f32b81165a5ef48531555b8e2
Yan Borges <yandsb16@gmail.com>
2021-11-04 12:37:56 -0400

    routes refactor

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -5,9 +5,20 @@ import { ISpecificationsRepository } from '../ISpecificationsRepository';
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
 
-  constructor() {
+  private static INSTANCE: SpecificationsRepository;
+
+  private constructor() {
     this.specifications = [];
   }
+
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+
+    return SpecificationsRepository.INSTANCE;
+  }
+
   create({ name, description }: ICreateSpecificationDTO): void {
     const specification = new Specification();
 
--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,22 +1,11 @@
 import { Request, Response, Router } from 'express';
 
-import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
-
 specificationsRoutes.post('/', (request: Request, response: Response) => {
-  const { name, description } = request.body;
-
-  const createSpecification = new CreateSpecificationService(
-    specificationsRepository
-  );
-
-  createSpecification.execute({ name, description });
-
-  return response.send();
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationsRoutes };
