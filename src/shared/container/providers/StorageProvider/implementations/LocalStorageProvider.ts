8d8eb880e968e468f8d2f4abac564bd7c03c2d2d
Yan Borges <yandsb16@gmail.com>
2021-11-17 09:49:18 -0400

    storage provider - local and aws s3

---
+++ src/shared/container/providers/StorageProvider/implementations/LocalStorageProvider.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import { resolve } from 'path';
+
+import uploadConfig from '@config/upload';
+
+import { IStorageProvider } from '../IStorageProvider';
+
+class LocalStorageProvider implements IStorageProvider {
+  async save(file: string, folder: string): Promise<string> {
+    await fs.promises.rename(
+      resolve(uploadConfig.tmpFolder, file),
+      resolve(`${uploadConfig.tmpFolder}/${folder}`, file)
+    );
+
+    return file;
+  }
+
+  async delete(file: string, folder: string): Promise<void> {
+    const fileName = resolve(`${uploadConfig.tmpFolder}/${folder}`, file);
+    try {
+      await fs.promises.stat(fileName);
+    } catch {
+      return;
+    }
+
+    await fs.promises.unlink(fileName);
+  }
+}
+
+export { LocalStorageProvider };
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -11,7 +11,7 @@ import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
-const uploadCarImages = multer(uploadConfig.upload('./tmp/cars'));
+const uploadCarImages = multer(uploadConfig);
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
